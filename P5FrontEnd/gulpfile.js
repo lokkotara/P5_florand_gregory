8ab6efb32ec8b30f4c0c6be759c193976f86ef3e
Grégory <milton-the-wizard@hotmail.fr>
2021-01-03 02:14:21 +0100

    avancée

--- P5FrontEnd/gulpfile.js
+++ P5FrontEnd/gulpfile.js
@@ -1,7 +1,10 @@
 "use strict";
 const ejs = require("gulp-ejs");
 const gulp = require("gulp");
- 
+const sass = require("gulp-sass");
+const browserSync = require("browser-sync");
+const minify = require("gulp-clean-css");
+
 /**
  * insert les partials dans les pages
  *
@@ -14,4 +17,27 @@ function makeTemplate(){
 }
 
 
-module.exports.makeTemplate = makeTemplate;
+// module.exports.makeTemplate = makeTemplate;
+
+function scss2css() {
+  return gulp.src("./scss/main.scss")
+    .pipe(sass.sync().on("error", sass.logError))
+    .pipe(minify())
+    .pipe(gulp.dest("./www/css"));
+}
+
+function autoUpdate(){
+  browserSync.init({
+    server: {
+      baseDir : "./www"
+    }
+  });
+  gulp.watch("./scss/main.scss", scss2css);
+  gulp.watch("./scss/elements/*.scss", scss2css);
+  gulp.watch("./www/").on("change", browserSync.reload);
+  gulp.watch("./src/templates/pages/*.html", makeTemplate);
+}
+
+exports.scss2css = scss2css;
+exports.watch = autoUpdate;
+exports.makeTemplate = makeTemplate;
