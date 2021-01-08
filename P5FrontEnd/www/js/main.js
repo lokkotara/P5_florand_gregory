3515b8757dcfe579a504fd7347129bba7b1ba9ea
Grégory <milton-the-wizard@hotmail.fr>
2021-01-08 03:04:00 +0100

    sauvegarde changement couleurs custom

--- P5FrontEnd/www/js/main.js
+++ P5FrontEnd/www/js/main.js
@@ -1,3 +1,4 @@
+/*eslint-disable no-unused-vars*/
 const orinoco = {
   dataManager : new DataManager("http://localhost:3000/api/teddies/")
 };
--- P5FrontEnd/www/js/pages/home.js
+++ P5FrontEnd/www/js/pages/home.js
@@ -1,4 +1,5 @@
-class Home{
+/*eslint-disable no-unused-vars*/
+class Home {
   /**
    * [constructor description]
    *
@@ -6,21 +7,24 @@ class Home{
    *
    * @constructor
    */
-  constructor(domTarget){
-    
+  constructor(domTarget) {
+
     this.showAllProducts(domTarget);
   }
 
-  async showAllProducts(domTarget){
+  async showAllProducts(domTarget) {
     const produits = await orinoco.dataManager.getAllProducts();
     console.log(produits);
     let content = "";
-    for (let i=0, size = produits.length; i<size; i++){
+    for (let i = 0, size = produits.length; i < size; i++) {
       content += this.productHtml(produits[i]);
     }
     domTarget.innerHTML = content;
 
   }
+
+  //génère les couleurs de personnalisation
+
 
   /**
    * génère le HTML d'un produit
@@ -29,7 +33,27 @@ class Home{
    *
    * @return  {String}         le html du produit
    */
-  productHtml(specs){
+  productHtml(specs) {
+    const colors = specs.colors;
+
+    // create a new div and set its attributes
+    let div = document.createElement('div');
+    div.className = 'div1';
+
+    for (let i = 0; i < colors.length; i++) {
+      const color = colors[i];
+      console.log(color);
+
+
+      // create a new text node and add it to the div
+      let span = document.createElement('span');
+      span.className = 'fas fa-circle';
+      span.style.color = color;
+      div.appendChild(span);
+
+      // add div to the document
+      document.body.appendChild(div);
+    }
     return `
     <article class="teddyCard">
         <a href="./produit.html">
@@ -38,9 +62,7 @@ class Home{
             <figcaption>
               <h3>${specs.name}</h3>
               <span class="displayColor">
-                <i class="fas fa-circle paleBrownColor"></i>
-                <i class="fas fa-circle darkBrownColor"></i>
-                <i class="fas fa-circle whiteColor"></i>
+                ${specs.colors}
               </span>
               <span class="price">${specs.price/100}€</span>
               <p>${specs.description}
