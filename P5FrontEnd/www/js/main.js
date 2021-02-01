c6401b9d2e7d1d0613ad0a29cee548919a8967b4
Grégory <milton-the-wizard@hotmail.fr>
2021-02-01 10:28:57 +0100

    Début commentaire de code

--- P5FrontEnd/www/js/main.js
+++ P5FrontEnd/www/js/main.js
@@ -11,11 +11,12 @@ orinoco.page = definePage();
  * @return  {Home|Product|Panier|Confirmation}  retourne une page
  */
 function definePage() {
-  let params = (new URL(document.location)).searchParams;
-  var url = window.location.pathname;
+  let params = (new URL(document.location)).searchParams; //récupère le paramètre de l'url. Utilisé pour récupérer l'id du produit à afficher sur la page produit.
+  var url = window.location.pathname; //récupère l'url de la page sur laquelle on est.
 
-  if (url == "/" || url == "/index.html") return new Home   (document.querySelector("div.cardsWrapper"));
-  if (url == "/produit.html")             return new Product(document.querySelector("div.singleCardWrapper"), params.get("_id"));
-  if (url == "/panier.html")              return new Panier (document.querySelector("tbody.cartSummaryBody"));
-  if (url == "/confirmation.html")       return new Confirmation (document.querySelector("main.mainConfirmation"));
+  // retourne l'instance de la classe appropriée en fonction de la page où l'on se trouve.
+  if (url == "/" || url == "/index.html") return new Home(document.querySelector("div.cardsWrapper"));
+  if (url == "/produit.html") return new Product(document.querySelector("div.singleCardWrapper"), params.get("_id"));
+  if (url == "/panier.html") return new Panier(document.querySelector("tbody.cartSummaryBody"));
+  if (url == "/confirmation.html") return new Confirmation(document.querySelector("main.mainConfirmation"));
 }
--- P5FrontEnd/www/js/pages/home.js
+++ P5FrontEnd/www/js/pages/home.js
@@ -13,15 +13,15 @@ class Home {
   }
 
   async showAllProducts(domTarget) {
-    const produits = await orinoco.dataManager.getAllProducts();
-    console.log(produits);
+    const produits = await orinoco.dataManager.getAllProducts(); //récupère un tableau de tous les produits de l'api.
     let content = "";
 
+    // créé une boucle qui applique la méthode productHtml à chaque itération trouvé dans produits.
     for (let i = 0, size = produits.length; i < size; i++) {
       content += this.productHtml(produits[i]);
     }
 
-    domTarget.innerHTML = content;
+    domTarget.innerHTML = content;//et l'affiche à l'endroit ciblé par le domTarget.
   }
 
   /**
@@ -32,7 +32,7 @@ class Home {
    * @return  {String}         le html du produit
    */
   productHtml(specs) {
-    return `
+    return /*html*/`
     <article class="teddyCard">
         <a href="./produit.html?_id=${specs._id}">
           <figure>
@@ -51,7 +51,7 @@ class Home {
   }
 
   /**
-   * génère la lise des couleurs
+   * génère la liste des couleurs sous forme de pastille.
    *
    * @param   {Array}  colors  les variantes
    *
@@ -60,6 +60,7 @@ class Home {
   showColor(colors) {
     let html = "";
 
+    // créé une boucle qui renvoit une pastille pour chaque couleur trouvé dans l'api.
     for (let i = 0, size = colors.length; i < size; i++) {
       html += `<i class="fas fa-circle ${this.convertToClassName(colors[i])}" ></i>`;
     }
@@ -67,18 +68,24 @@ class Home {
     return html;
   }
 
-
+  /**
+   * Récupère le nom de la couleur dans l'api et le transforme en nom de classe.
+   *
+   * @param   {string}  color  correspond aux couleurs pour chaque nounours
+   *
+   * @return  {string}         nom de classe en camelCase qui finit par Color.
+   */
   convertToClassName(color) {
     let colors = color
       .toLowerCase()
-      .split(" ");
+      .split(" ");//donne un tableau avec chaque mot en minuscule et séparé.
     let maj;
 
+    // 
     for (let i = 1, size = colors.length; i < size; i++) {
-      maj = colors[i].slice(0, 1).toUpperCase();
-      colors[i] = maj + colors[i].slice(1);
+      maj = colors[i].slice(0, 1).toUpperCase();//met une majuscule à la 1ère lettre de chaque mot.
+      colors[i] = maj + colors[i].slice(1);//colle le reste du mot en minuscule.
     }
-
-    return colors.join("") + "Color";
+    return colors.join("") + "Color";//renvoit le mot en camelCase avec Color à la fin.
   }
 }
